f744e89250d5892483aa02c6f1bfd8f4b899785a
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-05-29 00:41:10 +0400

    add class Users(Base)

---
+++ users_db.py
@@ -0,0 +1,18 @@
+from base import Base
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List, Optional
+
+
+class Users(Base):
+    __tablename__ = 'users_table'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    last_name: Mapped[str] = mapped_column(String(30))
+    first_name: Mapped[str] = mapped_column(String(30))
+    patronymic: Mapped[Optional[str]]
+    birthday: Mapped[str] = mapped_column(String(10))
+    age: Mapped[str] = mapped_column(String(3))
+    gender: Mapped[str] = mapped_column(String(10))
+    pay_method: Mapped[str] = mapped_column(String(10))
+
+    contacts: Mapped[List['Contacts']] = relationship(back_populates='users', cascade='all, delete_orphan')
